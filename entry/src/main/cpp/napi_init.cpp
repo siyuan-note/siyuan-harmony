4e85c1f26b8aaf5119d483869834485379d2943a
Daniel <845765@qq.com>
2024-11-22 19:54:02 +0800

    :tada: Init commit

---
+++ entry/src/main/cpp/napi_init.cpp
@@ -0,0 +1,53 @@
+#include "napi/native_api.h"
+#include "string.h"
+#include "thread"
+#include "libkernel.h"
+
+static char *value2String(napi_env env, napi_value value) {
+    size_t len = 0;
+    napi_get_value_string_utf8(env, value, nullptr, 0, &len);
+    char *buf = new char[len + 1];
+    napi_get_value_string_utf8(env, value, buf, len + 1, &len);
+    return buf;
+}
+
+static napi_value StartKernel0(napi_env env, napi_callback_info info) {
+    napi_value result;
+
+    size_t argc = 2;
+    napi_value args[2] = {nullptr};
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    std::thread t([&env, &args]() {
+        char *appDir = value2String(env, args[0]);
+        char *workspaceBaseDir = value2String(env, args[1]);
+
+        StartKernel((char *)"harmony", appDir, workspaceBaseDir, (char *)"Asia/Shanghai", (char *)"127.0.0.1",
+                    (char *)"zh_CN", (char *)"5.0");
+    });
+    t.join();
+
+    return result;
+}
+
+EXTERN_C_START
+static napi_value Init(napi_env env, napi_value exports) {
+    napi_property_descriptor desc[] = {
+        {"startKernel", nullptr, StartKernel0, nullptr, nullptr, nullptr, napi_default, nullptr},
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    return exports;
+}
+EXTERN_C_END
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "entry",
+    .nm_priv = ((void *)0),
+    .reserved = {0},
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void) { napi_module_register(&demoModule); }
---
+++ entry/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,1 @@
+export const startKernel: (appDir: string, workspaceBaseDir: string) => void;
