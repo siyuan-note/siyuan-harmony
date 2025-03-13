1a7f2514df224c55b2daff6c8a3db9824f0ae4fc
Daniel <845765@qq.com>
2025-03-13 10:15:59 +0800

    :art: Display IP address https://ld246.com/article/1741825235681

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -1,7 +1,25 @@
+/*
+ * SiYuan - 源于思考，饮水思源
+ * Copyright (c) 2020-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "libkernel.h"
 #include "napi/native_api.h"
 #include "string.h"
 #include "thread"
-#include "libkernel.h"
 
 static char *value2String(napi_env env, napi_value value) {
     size_t len = 0;
@@ -15,14 +33,14 @@ static napi_value StartKernel0(napi_env env, napi_callback_info info) {
     napi_value result;
 
     size_t argc = 2;
-    napi_value args[2] = {nullptr, nullptr};
+    napi_value args[3] = {nullptr, nullptr, nullptr};
     napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
     char *appDir = (value2String(env, args[0]));
     char *workspaceBaseDir = value2String(env, args[1]);
-
-    std::thread t([appDir, workspaceBaseDir]() {
-        StartKernel((char *)"harmony", appDir, workspaceBaseDir, (char *)"Asia/Shanghai", (char *)"127.0.0.1",
-                    (char *)"zh_CN", (char *)"5.0");
+    char *ip = value2String(env, args[2]);
+    std::thread t([appDir, workspaceBaseDir, ip]() {
+        StartKernel((char *)"harmony", appDir, workspaceBaseDir, (char *)"Asia/Shanghai", ip, (char *)"zh_CN",
+                    (char *)"5.0");
     });
     t.join();
 
@@ -33,11 +51,9 @@ static napi_value IsHttpServing0(napi_env env, napi_callback_info info) {
     napi_value result;
 
     GoUint8 ret;
-    std::thread t([&ret]() {
-        ret = IsHttpServing();
-    });
+    std::thread t([&ret]() { ret = IsHttpServing(); });
     t.join();
-    
+
     napi_create_uint32(env, ret, &result);
     return result;
 }
@@ -55,7 +71,7 @@ static napi_value DisableFeature0(napi_env env, napi_callback_info info) {
 }
 
 static napi_value Unzip0(napi_env env, napi_callback_info info) {
-     napi_value result;
+    napi_value result;
 
     size_t argc = 2;
     napi_value args[2] = {nullptr, nullptr};
@@ -63,9 +79,7 @@ static napi_value Unzip0(napi_env env, napi_callback_info info) {
     char *zipPath = value2String(env, args[0]);
     char *dest = value2String(env, args[1]);
 
-    std::thread t([zipPath, dest]() {
-        Unzip(zipPath, dest);
-    });
+    std::thread t([zipPath, dest]() { Unzip(zipPath, dest); });
     t.join();
 
     return result;
--- entry/src/main/cpp/types/libentry/Index.d.ts
+++ entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,4 +1,7 @@
-export const startKernel: (appDir: string, workspaceBaseDir: string) => void;
+export const startKernel: (appDir: string, workspaceBaseDir: string, ips: string) => void;
+
 export const isHttpServing: () => boolean;
+
 export const disableFeature: (feature: string) => void;
+
 export const unzip: (zipPath: string, dest: string) => void;
